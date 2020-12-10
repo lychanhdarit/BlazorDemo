73f96825e2ea99595fe125a7066d9ea3902d5460
lychanhdarit <lychanhdarit@gmail.com>
2020-12-10 08:58:08 +0700

    Auto commit: 12/10/2020 8:57:55 AM

---
+++ BlazorDemo/Todo/Data/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public string Note { get; set; }
+    }
+}
